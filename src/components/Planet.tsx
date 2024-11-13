b5994542fb7b3e020589b00bfb2f33f036b398b5
Michel Fournier <m.fournier.a@gmail.com>
2024-11-13 11:31:07 -0500

    initial completion

---
+++ src/components/Planet.tsx
@@ -0,0 +1,41 @@
+import { HTMLAttributes } from "react";
+import { cva } from "class-variance-authority";
+
+const classes = cva("bg-gradient-to-b to-gray-950  rounded-full", {
+  variants: {
+    size: {
+      sm: "size-4",
+      md: "size-6",
+      lg: "size-8",
+    },
+    color: {
+      violet: "from-violet-400",
+      teal: "from-teal-400",
+      fuchsia: "from-fuchsia-400",
+    },
+  },
+
+  defaultVariants: {
+    size: "lg",
+    color: "violet",
+  },
+});
+
+const Planet = (
+  props: {
+    size?: "sm" | "md" | "lg";
+    color?: "violet" | "teal" | "fuchsia";
+  } & HTMLAttributes<HTMLDivElement>
+) => {
+  return (
+    <div
+      className={classes({
+        size: props.size,
+        color: props.color,
+        className: props.className,
+      })}
+    ></div>
+  );
+};
+
+export default Planet;
--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,9 +1,13 @@
+"use client";
 import React from "react";
 import {
   faYoutube,
   faXTwitter,
   faDiscord,
 } from "@fortawesome/free-brands-svg-icons";
+import SectionBorder from "@/components/SectionBorder";
+import SectionContent from "@/components/SectionContent";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export const navItems = [
   {
@@ -20,7 +24,7 @@ export const navItems = [
   },
   {
     name: "Login",
-    href: "#login",
+    href: "#",
   },
 ];
 
@@ -28,22 +32,64 @@ export const socialLinks = [
   {
     name: "Youtube",
     icon: faYoutube,
-    href: "#youtube",
+    href: "#",
   },
   {
     name: "X",
     icon: faXTwitter,
-    href: "#x",
+    href: "#",
   },
   {
     name: "Discord",
     icon: faDiscord,
-    href: "#discord",
+    href: "#",
   },
 ];
 
 export const Footer = () => {
-  return <footer>footer section</footer>;
+  return (
+    <footer className="border-t border-[var(--color-border)]">
+      <div className="container py-8">
+        <div className="flex flex-col lg:flex-row lg:justify-between items-center gap-8">
+          <div className="font-extrabold text-2xl">sphereal.ai</div>
+          <nav className="flex flex-col md:flex-row gap-8 md:gap-16 items-center">
+            {navItems.map((item) => (
+              <a
+                key={item.name}
+                href={item.href}
+                className="uppercase text-xs tracking-widest font-bold text-gray-400"
+                onClick={(e) => {
+                  e.preventDefault();
+                  const element = document.querySelector(item.href);
+                  if (element) {
+                    element.scrollIntoView({
+                      behavior: "smooth",
+                    });
+                  }
+                }}
+              >
+                {item.name}
+              </a>
+            ))}
+          </nav>
+        </div>
+        <div className="flex flex-col md:flex-row-reverse md:justify-between items-center mt-16 gap-8">
+          <div className="flex justify-center gap-6">
+            {socialLinks.map((link) => (
+              <a href={link.href} key={link.name}>
+                <div className="size-10 rounded-full bg-gray-900 inline-flex items-center justify-center">
+                  <FontAwesomeIcon icon={link.icon} className="size-4" />
+                </div>
+              </a>
+            ))}
+          </div>
+          <p className="text-gray-500 text-sm">
+            &copy; MFDev, ALl right reserved.
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
